6a163a4f0364c147fc9155d0e01e513ff5f4182f
rokoDev <mishaDoter@gmail.com>
2017-07-16 04:38:52 +0300

    added GTest framework for ios

---
+++ proj.ios_mac/GTest/GTest.h
@@ -0,0 +1,19 @@
+//
+//  GTest.h
+//  GTest
+//
+//  Created by roko on 16/07/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GTest.
+FOUNDATION_EXPORT double GTestVersionNumber;
+
+//! Project version string for GTest.
+FOUNDATION_EXPORT const unsigned char GTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTest/PublicHeader.h>
+
+
