41385484a5045ec640d62869be9c14a243c72d7e
rokoDev <mishaDoter@gmail.com>
2017-07-17 23:43:56 +0300

    fix

--- scripts/prepare_for_mac.sh
+++ scripts/prepare_for_mac.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+cd cocos2d
+python download-deps.py
+
+cd ..
+
+git submodule init
+git submodule update
+
 cd proj.ios_mac
 #list all targets, build configurations, and schemes used in project
 xcodebuild -list -project Diabet.xcodeproj
@@ -7,7 +15,7 @@ xcodebuild -list -project Diabet.xcodeproj
 echo "Building test target for Mac OS: DiabetTest ..."
 
 #build test target for mac os
-OUTPUT=`xcodebuild -target DiabetTest -configuration Debug`
+OUTPUT=`xcodebuild -target DiabetTest -sdk macosx10.6 -configuration Debug`
 RESULT=`echo "$OUTPUT" | grep "\\*\\* BUILD "`
 if [ "$RESULT" != "** BUILD SUCCEEDED **" ]
 then
