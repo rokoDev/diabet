7bc5bf7eb7e17fe587c7d01fcef6f62c45d20703
rokoDev <mishaDoter@gmail.com>
2017-07-19 01:10:38 +0300

    deployment fix

--- scripts/prepare_for_mac.sh
+++ scripts/prepare_for_mac.sh
@@ -9,15 +9,23 @@ echo | python ./download-deps.py
 
 cd ..
 
-rm -rf publish
-
-cmake -E make_directory publish
+#rm -rf publish
+#
+#cmake -E make_directory publish
 
 cd proj.ios_mac
 #list all targets, build configurations, and schemes used in project
 xcodebuild -list -project Diabet.xcodeproj
 
+rm -rf build
+cmake -E make_directory build
+cd build
+
 echo "Building test target for Mac OS: DiabetTest ..."
+
+xcodebuild -project proj.ios_mac/Diabet.xcodeproj -target DiabetTest -configuration Debug CONFIGURATION_BUILD_DIR="../build"
+
+./build/DiabetTest
 
 #build test target for mac os
 #OUTPUT=`xcodebuild -target DiabetTest -sdk macosx10.6 -configuration Debug`
