656e4b3dd93de9388cc1217ad3e160f80aa691e9
rokoDev <mishaDoter@gmail.com>
2017-07-24 00:50:09 +0300

    minor fix

--- scripts/build_and_run_tests_mac_os.sh
+++ scripts/build_and_run_tests_mac_os.sh
@@ -3,18 +3,18 @@
 echo "start building tests..."
 xcodebuild -project ./proj.ios_mac/Diabet.xcodeproj -target DiabetTest -configuration Debug
 
-local build_status = $?
-echo "build_status >> $build_status"
+#local build_status = $?
+#echo "build_status >> $build_status"
 
-local status=$?
-if [ $build_status -ne 0 ]; then
-echo "error with $1" >&2
-return $build_status
-fi
-echo "test build succeeded"
+#local status=$?
+#if [ $build_status -ne 0 ]; then
+#echo "error with $1" >&2
+#return $build_status
+#fi
+#echo "test build succeeded"
 
 echo "running tests..."
 ./proj.ios_mac/build/Debug/DiabetTest
-local run_tests_status = $?
-echo "run_tests_status >> $run_tests_status"
-return $run_tests_status
+#local run_tests_status = $?
+#echo "run_tests_status >> $run_tests_status"
+#return $run_tests_status
